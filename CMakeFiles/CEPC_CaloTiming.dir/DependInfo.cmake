74e72828836a297e1468301322c741936e9a85d8
yihui-lai <yihuilai@foxmail.com>
2020-03-25 11:52:12 -0400

    add .mac

---
+++ CMakeFiles/CEPC_CaloTiming.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/users/yihuilai/cepc_layer/CEPC_CaloTiming.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/CEPC_CaloTiming.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/ConfigFile.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/ConfigFile.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/CreateTree.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/CreateTree.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/DetectorConstruction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/DetectorConstruction.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/EventAction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/EventAction.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/G4EmUserPhysics.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/G4EmUserPhysics.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/LedFiberTiming.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/LedFiberTiming.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/MyMaterials.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/MyMaterials.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/PrimaryGeneratorAction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/PrimaryGeneratorAction.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/RunAction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/RunAction.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/SteppingAction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/SteppingAction.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/SteppingVerbose.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/SteppingVerbose.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/TrackInformation.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/TrackInformation.cc.o"
+  "/data/users/yihuilai/cepc_layer/src/TrackingAction.cc" "/data/users/yihuilai/cepc_layer/CMakeFiles/CEPC_CaloTiming.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4GEOM_USE_USOLIDS"
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  "VECCORE_ENABLE_VC"
+  "VECGEOM_INPLACE_TRANSFORMATIONS"
+  "VECGEOM_NO_SPECIALIZATION"
+  "VECGEOM_QUADRILATERALS_VC"
+  "VECGEOM_ROOT"
+  "VECGEOM_USE_INDEXEDNAVSTATES"
+  "VECGEOM_VC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/cvmfs/sft.cern.ch/lcg/releases/clhep/2.4.1.0-2c56f/x86_64-slc6-gcc62-opt/lib/CLHEP-2.4.1.0/../../include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/Geant4"
+  "/cvmfs/sft.cern.ch/lcg/releases/XercesC/3.1.3-b3bf1/x86_64-slc6-gcc62-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/veccore/0.4.2-ff84f/x86_64-slc6-gcc62-opt/lib/cmake/VecCore/../../../include"
+  "/cvmfs/sft.cern.ch/lcg/releases/Vc/1.3.2-7fbe0/x86_64-slc6-gcc62-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.16.00-f8770/x86_64-slc6-gcc62-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/VecGeom/v1.1.0-1d606/x86_64-slc6-gcc62-opt/lib/cmake/VecGeom/../../../include"
+  "include"
+  "/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/clhep/2.4.1.0-2c56f/x86_64-slc6-gcc62-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/QtWidgets"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/QtGui"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/QtCore"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/./mkspecs/linux-g++"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/QtPrintSupport"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ template/condor-executable_2GeV_electron_N500_3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd /data/users/yihuilai/cepc_layer
+
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
+
+
+# setup software environment at UMD
+#
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/6.3/x86_64-slc6/setup.sh
+source /cvmfs/geant4.cern.ch/geant4/10.5/x86_64-slc6-gcc63-opt-MT/CMake-setup.sh
+export CXX=`which g++`
+export CC=`which gcc`
+export PATH=$PATH:/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.11.1/Linux-x86_64/bin
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/setup.sh
+source $ROOTSYS/bin/thisroot.sh
+#export LIBRARY_PATH=/data/users/yihuilai/dualReadout/fakelib:$LIBRARY_PATH
+#export LD_LIBRARY_PATH=/data/users/yihuilai/dualReadout/fakelib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/home/eno/dualReadout/fakelib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/eno/dualReadout/fakelib:$LD_LIBRARY_PATH
+echo $LIBRARY_path
+echo $LD_LIBRARY_PATH
+
+#
+# run 
+#
+
+
+ ./CEPC_CaloTiming -c template_ts.cfg -m run_2GeV_electron_N500.mac -o electron_2GeV_N500_3 > run_2GeV_electron_N500_3.log
+
+exitcode=$?
+
+
+#
+# end run
+#
+
+echo ""
+END_TIME=`/bin/date`
+echo "finished at $END_TIME"
+exit $exitcode
+
+
